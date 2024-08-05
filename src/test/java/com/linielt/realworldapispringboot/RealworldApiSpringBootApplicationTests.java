5b674f349092babc5518e35c5434baf8ead907ab
Linielt <115248908+Linielt@users.noreply.github.com>
2024-08-05 14:32:54 +0100

    Initial commit

---
+++ src/test/java/com/linielt/realworldapispringboot/RealworldApiSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.linielt.realworldapispringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealworldApiSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
